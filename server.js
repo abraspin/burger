043f68b6e3b7521d62580444b1f9cde8f6bccb7c
abraspin <abrahamspindel@gmail.com>
2020-10-19 13:38:48 -0500

    add server file

---
+++ server.js
@@ -0,0 +1,9 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
