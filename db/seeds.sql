6b409b5bd90ca08255feb29f7d5ae3172d7d1b14
abraspin <abrahamspindel@gmail.com>
2020-10-19 13:38:29 -0500

    add sql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("She's a Super Leek", true), 
+("New Bacon-ings",true), 
+("Never Been Feta",true), 
+("The final Krautdown",false), 
+("If looks could Kale",False), 
+("Little Swiss Bunshine",true);
