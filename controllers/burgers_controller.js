f8a3b85648b8a5cedb57a1603c7e1e6dbcd50e72
abraspin <abrahamspindel@gmail.com>
2020-10-19 14:41:29 -0500

    add burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    const viewData = {
+      burgers: data,
+    };
+    console.log(viewData);
+    res.render("index", viewData);
+  });
+});
+
+router.post("/", (req, res) => {
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
+    // Send back the ID of the new quote
+    //TODO: what's this?
+    res.json({ id: result.insertId });
+  });
+});
+
+router.updateOne("/", (request, response) => {
+  const condition = { devoured: request.body.devoured };
+
+  console.log("condition", condition);
+
+  burger.update({ devoured: request.body.devoured }, condition, (result) => {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return response.status(404).end();
+    }
+    response.status(200).end();
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
